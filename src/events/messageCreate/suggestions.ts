b051a5264bfcb48099bdc9d3ffab4baf70b4f2ec
Mel <memyselfandlie@gmail.com>
2025-08-20 16:13:43 -0400

    feat: add thumbs to carl-reports

--- src/events/messageCreate/suggestions.ts
+++ src/events/messageCreate/suggestions.ts
@@ -1,11 +1,13 @@
 import { Message } from 'discord.js';
 
 export async function suggestionsHandler(message: Message): Promise<void> {
-  if (message.author.bot) {
-    return;
-  }
+  // if (message.author.bot) {
+  //   return;
+  // }
 
-  if (message.channel.id !== '1276915305205207154') {
+  // 8/20/25 - admins asked me to add this to carl-reports, so disabled bot check. I'm not making a whole ass new file for the same functionality sorry.
+
+  if (message.channel.id !== '1276915305205207154' && message.channel.id !== '754491912190427207') {
     return;
   }
 
