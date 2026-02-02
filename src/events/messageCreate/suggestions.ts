5dd645cbf9d53e96d5e0e99ec3533fb5dd8634c7
Mel <memyselfandlie@gmail.com>
2026-02-02 14:58:13 -0500

    Remove channel ID check for specific channel

--- src/events/messageCreate/suggestions.ts
+++ src/events/messageCreate/suggestions.ts
@@ -7,7 +7,7 @@ export async function suggestionsHandler(message: Message): Promise<void> {
 
   // 8/20/25 - admins asked me to add this to carl-reports, so disabled bot check. I'm not making a whole ass new file for the same functionality sorry.
 
-  if (message.channel.id !== '1276915305205207154' && message.channel.id !== '754491912190427207') {
+  if (message.channel.id !== '754491912190427207') {
     return;
   }
 
